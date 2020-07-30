dee0f4999d5be9ce6f79fd900829c0c1fcdfbb37
UStuej <bushytail4@gmail.com>
2020-07-30 15:42:43 -0500

    Init MATE-ROV project
    Added a placeholder Main.java file to initialize the general structure of the project.

---
+++ java/MATE-ROV/src/com/dorobotics/materov/y2020/Main.java
@@ -0,0 +1,11 @@
+package com.dorobotics.materov.y2020;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is a placeholder Main.java file");
+		
+	}
+	
+}
